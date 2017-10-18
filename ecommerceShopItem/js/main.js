b1ea85749d0fbeed186ab5038c988b868cd4c2bc
Darren Poon <dyhpoon@gmail.com>
2017-10-18 17:46:36 +0800

    format code

--- ecommerceShopItem/js/main.js
+++ ecommerceShopItem/js/main.js
@@ -9,7 +9,7 @@ function background(c1, c2) {
 }
 
 function changeBg(c1, c2) {
-    $('div.bg').css(background(c1, c2)).fadeIn(700, function () {
+    $('div.bg').css(background(c1, c2)).fadeIn(700, function() {
         $('body').css(background(c1, c2));
         $('bg').hide();
     })
@@ -23,19 +23,19 @@ $slider.slick({
     speed: 600,
     fade: true,
     focusOnSelect: true,
-    customPaging: function (slider, i) {
+    customPaging: function(slider, i) {
         var color = $(slider.$slides[i]).data('color').split(',')[1];
         return '<a><svg width="100%" height="100%" viewBox="0 0 16 16"><circle cx="8" cy="8" r="6.215" stroke="' + color + '"></circle></svg><span style="background:' + color + '"></span></a>';
     }
-}).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+  }).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
     var colors = $('figure', $slider).eq(nextSlide).data('color').split(',');
     var color1 = colors[0];
     var color2 = colors[1];
     $('.price, .btn').css({
-        color: color1
+      color: color1
     });
     changeBg(color1, color2);
     $('.btn').css({
-        borderColor: color2
+      borderColor: color2
     });
-});
+  });
